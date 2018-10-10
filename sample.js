1bf571d02e243fcbbc0a34eb23c52eadd3209ea0
Rob Foulkrod <rob.foulkrod@nhls.com>
2018-10-10 14:39:12 -0400

    Mid dad wednesday

---
+++ sample.js
@@ -0,0 +1,7 @@
+javascript: (function () { 
+    $(".completionStatusList:visible")
+    .filter(function (index) 
+    { var value = +$(this).val();
+         var arrayPosition = [0].indexOf(value); var shouldChange = arrayPosition >= 0;
+          return shouldChange;
+         }).val(10).trigger('change'); })()
